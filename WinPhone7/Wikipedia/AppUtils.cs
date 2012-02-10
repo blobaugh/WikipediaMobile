329a4a7fe4cee40f9946445761c005552b635a1a
Corey Brookes <csbrookes@gmail.com>
2012-02-10 11:07:30 -0800

    AppUtils

---
+++ WinPhone7/Wikipedia/AppUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using WP7GapClassLib.PhoneGap;
+using WP7GapClassLib.PhoneGap.Commands;
+using WP7GapClassLib.PhoneGap.JSON;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+using Wikipedia;
+
+namespace PhoneGap.Extension.Commands
+{
+    public class AppUtils : BaseCommand
+    {
+        public class ApplicationExitException : System.Exception
+        {
+        }
+
+        public void exit(string args)
+        {
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                throw new ApplicationExitException();
+            });
+        }
+    }
+}
